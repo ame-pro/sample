09326a53561d9392b88f5805eb9ec33b1187b74f
ame <ti.amo999@nifty.com>
2020-12-19 13:46:10 +0900

    コード変更

--- daikei.php
+++ daikei.php
@@ -1,1 +1,27 @@
-My Project
+<?php
+function trapezoid($top, $bottom, $height) {
+	$area = ($top + $bottom) * $height / 2;
+	return $area;
+}
+$top = 10;
+$bottom = 20;
+$height = '';
+if (is_numeric($top) && is_numeric($bottom) && is_numeric($height)) {
+	$area = trapezoid($top, $bottom, $height);
+} else {
+	$area = null;
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>台形の面積</title>
+</head>
+<body>
+<p>上底<?= $top; ?></p>
+<p>下底<?= $bottom; ?></p>
+<p>高さ<?= $height; ?></p>
+<p>面積<?= $area; ?></p>
+</body>
+</html>
